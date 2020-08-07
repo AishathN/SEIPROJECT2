62e541c542e34d2e0d8bc4ade8c46626637bab34
Aishath Nasir <aishath_n@hotmail.com>
2020-08-07 01:16:55 +0100

    implemented checkmark hide when clicked, as well as reshowing

--- src/components/cocktails/Cocktail.js
+++ src/components/cocktails/Cocktail.js
@@ -11,6 +11,7 @@ class Cocktail extends React.Component{
     booze: null,
     message: ' ',
     measurements: ' ',
+    classbutton: 'buttonImage'
   }
   async componentDidMount() {
     try {
@@ -23,7 +24,6 @@ class Cocktail extends React.Component{
   getDrink = async() => {
     try {
       const res = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-      // console.log({ booze: res.data })
       this.setState({ booze: res.data })
       this.setState({ message: 'All you need to know: It has ' +  this.state.booze.drinks[0].strIngredient1 
       + ' and ' +  this.state.booze.drinks[0].strIngredient2 })
@@ -33,14 +33,16 @@ class Cocktail extends React.Component{
   }
 
   clickDrink = () => {
+    this.setState({ classbutton: 'buttonImage' })
     this.setState({ measurements: '' })
     return (this.getDrink())
   }
   getRecipe = () => {
+    this.setState({ classbutton: 'invisible' })
     // this is a cheeky placeholder after all the failed maps, filters and for loops
     this.setState({ message: this.state.booze.drinks[0].strInstructions })
     this.setState({ measurements: this.state.booze.drinks[0].strIngredient1 + ' ' + this.state.booze.drinks[0].strMeasure1 + 
-    ' , ' +  this.state.booze.drinks[0].strIngredient2 + ' ' + this.state.booze.drinks[0].strMeasure2 + ' '})
+    ' , ' +  this.state.booze.drinks[0].strIngredient2 + ' ' + this.state.booze.drinks[0].strMeasure2 + ' ' })
   }
   
   render(){
@@ -55,7 +57,7 @@ class Cocktail extends React.Component{
               <div><h4>{this.state.message}<p>{this.state.measurements}</p></h4></div>
               <div className="buttonDiv">
                 <img className ="buttonImage" src={cross} width="50" height="50" onClick={this.clickDrink}></img>
-                <img className ="buttonImage" src={check} width="50" height="50"  onClick={this.getRecipe}></img>
+                <img id ="checkmark" className ={this.state.classbutton} src={check} width="50" height="50"  onClick={this.getRecipe}></img>
               </div>
             </div>
           </div>
